b6caeedc0138418270af31da3e1822b33cd5d83c
Matheus Santo <mathe@192.168.0.2>
2017-04-15 13:16:35 -0300

    FactoryConnection, Conexões com o Banco.

---
+++ projetoautomata/src/main/java/br/senac/tads3/pi03b/projetoautomata/resources/automata.sql
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  matheus_santo
+ * Created: Apr 15, 2017
+ */
+
+CREATE DATABASE automata;
