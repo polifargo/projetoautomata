ff43b09f0d86933d7249338bfcc62d38f3a26af1
Thaís Matos <thais.smatos@outlook.com.br>
2017-05-16 16:23:49 -0300

    Unidades e técnicos

--- projetoautomata/src/main/java/br/senac/tads3/pi03b/projetoautomata/resources/script_dados.sql
+++ projetoautomata/src/main/java/br/senac/tads3/pi03b/projetoautomata/resources/script_dados.sql
@@ -9,4 +9,16 @@ insert into clientes (nome, apelido, tipo, uf, cep, cidade, logradouro, bairro, 
 insert into clientes (nome, apelido, tipo, uf, cep, cidade, logradouro, bairro, email, telefone) values ('leandro escobar', 'leandro escobar', 'f', 'sp', '01452-001', 'são paulo', 'avenida brigadeiro faria lima, 1663 - 1º andar', 'jardim paulistano', 'leandroescobar@cetip.com.br', '31111962');
 insert into clientes (nome, apelido, tipo, uf, cep, cidade, logradouro, bairro, email, telefone) values ('aline domingues lorusso', 'aline domingues loru', 'f', 'sp', '12233-002', 'são josé dos campos', 'avenida cidade jardim, 3141 - casa 102', 'bosque dos eucaliptos', 'li_lorusso@hotmail.com', '39361833');
 insert into clientes (nome, apelido, tipo, uf, cep, cidade, logradouro, bairro, email, telefone) values ('renato garcia gil', 'renato garcia gil', 'f', 'rs', '06070-130', 'porto alegre', 'rua joaquim jacinto, 120 -', 'bela vista', 'rggil@uol.com.br', '1198982858');
-insert into clientes (nome, apelido, tipo, uf, cep, cidade, logradouro, bairro, email, telefone) values ('antonio denisson martins barbosa', 'antonio denisson mar', 'j', 'rs', '14784-200', 'porto alegre', 'via conselheiro antonio prado, 1400 - dep. financeiro', 'pedro cavalini', 'denisson_445@hotmail.com', '1778123731')
+insert into clientes (nome, apelido, tipo, uf, cep, cidade, logradouro, bairro, email, telefone) values ('antonio denisson martins barbosa', 'antonio denisson mar', 'j', 'rs', '14784-200', 'porto alegre', 'via conselheiro antonio prado, 1400 - dep. financeiro', 'pedro cavalini', 'denisson_445@hotmail.com', '1778123731');
+
+insert into Unidades (id, fantasia, razao, uf, cep, cidade, logradouro, bairro, CadastroNacional) values ('ESP', 'Unidade SP', 'UNIDADE DE SÃO PAULO LTDA', 'SP', 'SÃO PAULO', 'AV PAULISTA, 2002', 'CERQUEIRA CESAR', '56259656000188');
+insert into Unidades (id, fantasia, razao, uf, cep, cidade, logradouro, bairro, CadastroNacional) values ('EPE', 'Unidade PE', 'UNIDADE DE PERNAMBUCO LTDA', 'PE', 'PERNAMBUCO', 'AV PERNAMBUCANA, 564', 'PANELAS', '56259656000269');
+insert into Unidades (id, fantasia, razao, uf, cep, cidade, logradouro, bairro, CadastroNacional) values ('ERS', 'Unidade RS', 'UNIDADE DO RIO GRANDE DO S LTDA', 'RS', 'RIO GRANDE DO S', 'AV RIO GRANDE, 22', 'FLORIPA', '56259656000340');
+
+insert into TipoServico (id, Descricao) values (1, 'Conserto de notebooks');
+insert into TipoServico (id, Descricao) values (2, 'Upgrade equipamento');
+insert into TipoServico (id, Descricao) values (3, 'Upgrade software');
+
+insert into Tecnico(id, NomeTecnico, TipoServPreferencial) values (1, 'Renato', 3);
+insert into Tecnico(id, NomeTecnico, TipoServPreferencial) values (2, 'Carlos', 1);
+insert into Tecnico(id, NomeTecnico, TipoServPreferencial) values (3, 'Vitor', 2);
